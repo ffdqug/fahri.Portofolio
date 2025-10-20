c6cd8302e992f1a8163c926cd421de0f535fa748
ffdqug <naksnskpakao@gmail.com>
2025-10-20 13:46:54 +0700

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -4,26 +4,18 @@ export default {
     return {
       activeTab: 1,
       items: [
+        
         {
           id: 1,
-          name: 'Personal Website',
-          imageUrl: 'portfolio_v2',
-          status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
-          tech: 'VueJS 3, Tailwind',
-          github: 'https://github.com/rakha-elctrnx/Portofolio-v2',
-          demo: 'https://ahmad fahri.netlify.app/'
-        },
-        {
-          id: 2,
           name: 'Sahabat Muslim',
           imageUrl: 'sahabatmuslim',
           status: 'A website that has a responsive display to make it easier for Muslims to read the Quran from anywhere.',
           tech: 'VueJS 3, Tailwind',
-          github: 'https://github.com/rakha-elctrnx/muslim-companion',
+          github:
           demo: 'https://sahabat-muslim.netlify.app/'
         },
         {
-          id: 3,
+          id: 2,
           name: 'SISTEKDES',
           imageUrl: 'sistekdes',
           status: 'Sistem Informasi dan Teknologi Desa, a web application to assist village government work in administration, population data management, community services, etc.',
@@ -32,7 +24,7 @@ export default {
           demo: 'null'
         },
         {
-          id: 4,
+          id: 3,
           name: 'Bumdesma',
           imageUrl: 'bumdesma',
           status: 'a web application used by village-owned enterprises at the sub-district level to make it easier for them in terms of accounting that is equipped with a microfinance balance.',
@@ -41,7 +33,7 @@ export default {
           demo: 'null'
         },
         {
-          id: 5,
+          id: 4,
           name: 'SP2SE',
           imageUrl: 'sp2se',
           status: 'Sistem Percepatan Pelayanan Surat Elektronik, A web application to manage letters in the village.',
